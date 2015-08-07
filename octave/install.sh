a7c9d40d0e9080e32fcce6f0ede0e20d25c503aa
David J. H. Shih <djh.shih@gmail.com>
2015-08-07 02:10:43 -0400

    Added octave.

---
+++ octave/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Script for installing python
+
+# Prerequisites: - gcc
+#                - wget
+
+name="octave"
+version="4.0.0"
+target_dir=$ROOT
+package=$name-$version
+
+# download package
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+wget -O $package.tar.gz ftp://ftp.gnu.org/gnu/octave/$package.tar.gz
+mkdir $package
+tar -xzf $package.tar.gz -C $package --strip-components=1
+cd $package
+
+# extract files, configure, and compile
+
+./configure --prefix=$target_dir
+make
+make install
+
