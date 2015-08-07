4648a5a9143745aa73a9663418bde04e0c636d45
David J. H. Shih <djh.shih@gmail.com>
2015-08-07 11:43:43 -0400

    Added r.

---
+++ r/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Script for installing python
+
+# Prerequisites: - gcc
+#                - wget
+
+name="r"
+version="3.2.1"
+target_dir=$ROOT
+package=$name-$version
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+# download package
+
+wget -O $package.tar.gz https://cran.r-project.org/src/base/R-3/R-${version}.tar.gz
+
+# extract files, configure, and compile
+
+mkdir $package
+tar -xzf $package.tar.gz -C $package --strip-components=1
+cd $package
+
+
+./configure --prefix=$target_dir
+make
+make install
+
