d705dd5987bb622e5cb166809f5dea7422490deb
David J. H. Shih <djh.shih@gmail.com>
2015-08-07 02:08:20 -0400

    Simplify PATH expression to .bashrc. Create share/info and share/man.

--- local/install.sh
+++ local/install.sh
@@ -1,11 +1,13 @@
 # define ROOT unless previously defined
 export ROOT=${ROOT:-$HOME/local}
 
-mkdir -p $ROOT/{bin,lib,share}
+mkdir -p $ROOT/{bin,lib,share/man,share/info}
 
-echo "export ROOT=$ROOT" >> $HOME/.bashrc
-# use single quotes for literal printing
+echo 'export ROOT=$ROOT' >> $HOME/.bashrc
 echo 'export PATH=$ROOT/bin:$PATH' >> $HOME/.bashrc
 echo 'export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH' >> $HOME/.bashrc
+echo 'export MANPATH=$ROOT/share/man:$MANPATH' >> $HOME/.bashrc
+echo 'export INFOPATH=$ROOT/share/info:$INFOPATH' >> $HOME/.bashrc
 
 . $HOME/.bashrc
+
