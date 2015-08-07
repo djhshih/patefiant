de62627dc02d79558c67ef6bc21747fd5a8cba29
David J. H. Shih <djh.shih@gmail.com>
2015-08-07 02:09:41 -0400

    Added installation for R-devel.

---
+++ r-devel/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Script for installing python
+
+# Prerequisites: - gcc
+#                - wget
+
+
+name="r"
+version="devel"
+target_dir=$ROOT
+package=$name-$version
+
+# download package
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+wget -O $package.tar.gz ftp://ftp.stat.math.ethz.ch/Software/R/R-${version}.tar.gz
+mkdir $package
+tar -xzf $package.tar.gz -C $package --strip-components=1
+cd $package
+
+# extract files, configure, and compile
+
+./configure --prefix=$target_dir
+make
+make install
+
