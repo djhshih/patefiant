e0010f01ab4734960ae20a582fd13580b88c34c4
David J. H. Shih <djh.shih@gmail.com>
2015-08-08 20:55:37 -0400

    Fixed root path.

--- local/install.sh
+++ local/install.sh
@@ -3,7 +3,7 @@ export ROOT=${ROOT:-$HOME/local}
 
 mkdir -p $ROOT/{bin,lib,share/man,share/info}
 
-echo 'export ROOT=$ROOT' >> $HOME/.bashrc
+echo 'export ROOT=$HOME/local' >> $HOME/.bashrc
 echo 'export PATH=$ROOT/bin:$PATH' >> $HOME/.bashrc
 echo 'export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH' >> $HOME/.bashrc
 echo 'export MANPATH=$ROOT/share/man:$MANPATH' >> $HOME/.bashrc
