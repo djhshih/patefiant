4eec765be41ab8162efad2949f1e8e28dd1467ee
David J. H. Shih <djh.shih@gmail.com>
2016-01-05 20:48:59 -0500

    Added openfst.

---
+++ pkg/openfst/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+# Script for installing python
+
+# Prerequisites: - gcc
+#                - wget
+
+name="openfst"
+version="1.3.2"
+target_dir=$ROOT
+package=$name-$version
+
+# download package
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+wget -O $package.tar.gz http://www.openfst.org/twiki/pub/FST/FstDownload/$package.tar.gz
+mkdir $package
+tar -xzf $package.tar.gz -C $package --strip-components=1
+cd $package
+
+# extract files, configure, and compile
+
+./configure --prefix=$target_dir \
+	--enable-far --enable-pdt
+make
+make install
+
+mv $target_dir/lib/fst/* $target_dir/lib && rmdir $target_dir/lib/fst
+
+cd -
+
