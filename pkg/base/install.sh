a095acecc27de4904a9c8031dd9a48a2066012ee
David J. H. Shih <djh.shih@gmail.com>
2016-01-06 18:44:40 -0500

    Move bashrc addendum to a separate file.

--- pkg/base/install.sh
+++ pkg/base/install.sh
@@ -1,39 +1,26 @@
 #!/bin/bash 
 set -o errexit
 
+basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p $ROOT/{bin,lib,share/man,share/info,opt}
 
 
-cat >>$HOME/.bashrc <<EOL
+cat >>$HOME/.bashrc <<'EOL'
 
 #### BEGIN PATEFIANT ####
 
-export ROOT=$ROOT
-EOL
-
-cat >>$HOME/.bashrc <<'EOL'
-
-# compile time environmental variables
-export CPATH=$ROOT/include:$CPATH
-export LIBRARY_PATH=$ROOT/lib:$LIBRARY_PATH
-
-# run time environmental variables
-export PATH=$ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH
-export MANPATH=$ROOT/share/man:$MANPATH
-export INFOPATH=$ROOT/share/info:$INFOPATH
-
-LPATH=''
-for pkg in $ROOT/opt/*; do
-	if [[ -d $pkg/bin ]]; then
-		LPATH+=:$pkg/bin
-	else
-		LPATH+=:$pkg
-	fi
-done
-export PATH=$PATH:$LPATH
+if [[ -f $HOME/.patefiant.bashrc ]]; then
+	 . $HOME/.patefiant.bashrc
+fi
 
 #### END PATEFIANT ####
 
 EOL
+
+cat >$HOME/.patefiant.bashrc <<EOL
+export PATEFIANT_ROOT=$ROOT
+EOL
+
+cat $basepath/bashrc >>$HOME/.patefiant.bashrc
 
