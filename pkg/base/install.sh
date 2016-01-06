fc70484de12b12ad998355ec08314db2584bf398
David J. H. Shih <djh.shih@gmail.com>
2016-01-06 15:19:39 -0500

    Fixed error regarding empty match.

--- pkg/base/install.sh
+++ pkg/base/install.sh
@@ -3,9 +3,15 @@ set -o errexit
 
 mkdir -p $ROOT/{bin,lib,share/man,share/info,opt}
 
-cat >>$HOME/.bashrc <<'EOL'
+
+cat >>$HOME/.bashrc <<EOL
 
 #### BEGIN PATEFIANT ####
+
+export ROOT=$ROOT
+EOL
+
+cat >>$HOME/.bashrc <<'EOL'
 
 # compile time environmental variables
 export CPATH=$ROOT/include:$CPATH
@@ -17,15 +23,17 @@ export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH
 export MANPATH=$ROOT/share/man:$MANPATH
 export INFOPATH=$ROOT/share/info:$INFOPATH
 
-LPATH=''
-for pkg in $ROOT/opt/*; do
-	if [[ -d $pkg/bin ]]; then
-		LPATH+=:$pkg/bin
-	else
-		LPATH+=:$pkg
-	fi
-done
-export PATH=$PATH:$LPATH
+if [ $(ls -A $ROOT/opt) ]; then
+	LPATH=''
+	for pkg in $ROOT/opt/*; do
+		if [[ -d $pkg/bin ]]; then
+			LPATH+=:$pkg/bin
+		else
+			LPATH+=:$pkg
+		fi
+	done
+	export PATH=$PATH:$LPATH
+fi
 
 #### END PATEFIANT ####
 
