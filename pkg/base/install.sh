79e6defcbf3b320c21c2503bb2638f35c1a4906d
David J. H. Shih <djh.shih@gmail.com>
2016-01-06 14:58:59 -0500

    Fixed major path error.

--- pkg/base/install.sh
+++ pkg/base/install.sh
@@ -8,14 +8,14 @@ cat >>$HOME/.bashrc <<'EOL'
 #### BEGIN PATEFIANT ####
 
 # compile time environmental variables
-export CPATH='$ROOT/include:$CPATH'
-export LIBRARY_PATH='$ROOT/lib:$LIBRARY_PATH'
+export CPATH=$ROOT/include:$CPATH
+export LIBRARY_PATH=$ROOT/lib:$LIBRARY_PATH
 
 # run time environmental variables
-export PATH='$ROOT/bin:$PATH >> $HOME/.bashrc'
-export LD_LIBRARY_PATH='$ROOT/lib:$LD_LIBRARY_PATH'
-export MANPATH='$ROOT/share/man:$MANPATH'
-export INFOPATH='$ROOT/share/info:$INFOPATH'
+export PATH=$ROOT/bin:$PATH
+export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH
+export MANPATH=$ROOT/share/man:$MANPATH
+export INFOPATH=$ROOT/share/info:$INFOPATH
 
 LPATH=''
 for pkg in $ROOT/opt/*; do
