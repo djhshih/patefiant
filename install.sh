d66a9cdb7d7629426248a8aa759281a3d461f88d
David J. H. Shih <djh.shih@gmail.com>
2016-01-06 14:52:16 -0500

    Make installation script more cross-platform. Install proot by default.

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -o errexit
-set -o nounset
 
-homepath="$(readlink -f "$0")"
-basepath=${homepath%/*}
+baseath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # set ROOT to $HOME/local unless already defined
 export ROOT=${ROOT:-$HOME/local}
 
-#$basepath/pkg/base/install.sh
-#$basepath/pkg/proot/install.sh
+$basepath/pkg/base/install.sh
+$basepath/pkg/proot/install.sh
 
 sed "s|basepath=.*|basepath=$basepath|" $basepath/bin/fac > $ROOT/bin/fac
 chmod +x $ROOT/bin/fac
-
-fac install base
 
 . $HOME/.bashrc
 
