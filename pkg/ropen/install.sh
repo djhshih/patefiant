e15d4096f7db0b7403e5f1310e7570c6572b1328
David J. H. Shih <djh.shih@gmail.com>
2018-06-14 18:31:13 -0500

    Added ropen.

---
+++ pkg/ropen/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Script for installing ropen
+
+name="ropen"
+version="0.2"
+target_dir=$PATEFIANT_ROOT
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+# install rip
+curl -L https://github.com/djhshih/ropen/archive/v${version}.tar.gz |
+	tar -xz --strip-components=1 --wildcards 'ropen-*/ropen'
+install ropen $target_dir/bin && rm ropen
+
