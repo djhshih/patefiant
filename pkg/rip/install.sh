c29a56e592c7340fce8a15db0777b9337117410e
David J. H. Shih <djh.shih@gmail.com>
2019-03-12 12:51:12 -0500

    Fix rip and ropen installation for BSD.

--- pkg/rip/install.sh
+++ pkg/rip/install.sh
@@ -10,6 +10,7 @@ target_dir=$PATEFIANT_ROOT
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
 
 # install rip
-curl -L https://github.com/djhshih/rip/archive/v${version}.tar.gz | tar -xz --strip-components=1 --wildcards 'rip-*/rip'
+curl -L https://github.com/djhshih/rip/archive/v${version}.tar.gz |
+	tar -xz --strip-components=1 'rip-*/rip'
 install rip $target_dir/bin
 
