98c87f7a057852b7643c758ba6c74be9c7f8544b
David J. H. Shih <djh.shih@gmail.com>
2019-11-06 10:00:47 -0600

    Changed rust install path.

--- pkg/rust/install.sh
+++ pkg/rust/install.sh
@@ -5,15 +5,17 @@ set -o nounset
 
 localdir=$(dirname "$(readlink -f "$0")")
 
+name=rust
 version=latest
+target_dir=$PATEFIANT_ROOT/opt/rust
 
 # pre-emptively create rust directories and establish symlinks at 
 # standard rust locations
 dirs=( .cargo .rustup .multirust )
 for f in ${dirs[@]}; do
-	dir=$PATEFIANT_ROOT/rust/$f
+	dir=$target_dir/$f
 	mkdir -p $dir
-	ln -s $dir $HOME/$f
+	ln -sf $dir $HOME/$f
 done
 
 # Download and install rust interactively
