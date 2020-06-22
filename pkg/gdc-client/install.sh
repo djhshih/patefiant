724947991ff5497342c6d447760fe622950e4320
David J. H. Shih <djh.shih@gmail.com>
2020-06-22 14:29:31 -0500

    Update gdc-client.

--- pkg/gdc-client/install.sh
+++ pkg/gdc-client/install.sh
@@ -2,12 +2,12 @@
 set -eou pipefail
 
 name=gdc-client
-version=1.3.0
+version=1.5.0
 url=https://gdc.cancer.gov/system/files/authenticated%20user/0
 
 target_dir=$PATEFIANT_ROOT
 
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
-curl -o $name.zip $url/${name}_v${version}_Ubuntu14.04_x64.zip
+curl -o $name.zip $url/${name}_v${version}_Ubuntu_x64.zip
 unzip $name.zip -d $target_dir/bin
 
