ab5de1c0eee2c21bda6f6ecbeeed4c6fe9e192f3
David J. H. Shih <djh.shih@gmail.com>
2020-06-22 23:03:09 -0500

    Added support for OSX for gdc-client.

--- pkg/gdc-client/install.sh
+++ pkg/gdc-client/install.sh
@@ -5,9 +5,20 @@ name=gdc-client
 version=1.5.0
 url=https://gdc.cancer.gov/system/files/authenticated%20user/0
 
+case "$(uname -s)" in
+	Linux*)     platform=Ubuntu;;
+	Darwin*)    platform=OSX;;
+	*)          platform=;;
+esac 
+
+if [[ -z $platform ]]; then
+	echo "Unsupported platform"
+	exit 1
+fi
+
 target_dir=$PATEFIANT_ROOT
 
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
-curl -o $name.zip $url/${name}_v${version}_Ubuntu_x64.zip
+curl -o $name.zip $url/${name}_v${version}_${platform}_x64.zip
 unzip $name.zip -d $target_dir/bin
 
