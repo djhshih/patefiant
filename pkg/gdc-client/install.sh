ec1ac6d52a36809f1b4dc57a0df83ae8c5a0cb71
David J. H. Shih <djh.shih@gmail.com>
2020-08-07 11:45:15 -0500

    Return to original directory.

--- pkg/gdc-client/install.sh
+++ pkg/gdc-client/install.sh
@@ -19,6 +19,8 @@ fi
 target_dir=$PATEFIANT_ROOT
 
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
 curl -o $name.zip $url/${name}_v${version}_${platform}_x64.zip
 unzip $name.zip -d $target_dir/bin
 
+cd -
