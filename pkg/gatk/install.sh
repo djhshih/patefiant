c03b83300fa3fcd44353449691ed7f1fd1932ac6
David J. H. Shih <djh.shih@gmail.com>
2021-03-02 16:52:01 -0600

    Added gatk.

---
+++ pkg/gatk/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+localdir=$(dirname "$(readlink -f "$0")")
+
+name=gatk
+version=4.1.9.0
+target_dir=$PATEFIANT_ROOT/opt/gatk
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+# Download and extract
+
+wget https://github.com/broadinstitute/gatk/releases/download/$version/$name-$version.zip
+unzip gatk-*.zip && rm gatk-*.zip
+mv gatk-* $target_dir
+
+cd -
+
