56c43213003423abe306c0debdfdfaca48a9de87
David J. H. Shih <djh.shih@gmail.com>
2021-07-30 15:55:00 -0400

    Removed extra url.

--- pkg/aws-cli/install.sh
+++ pkg/aws-cli/install.sh
@@ -10,7 +10,6 @@ version=
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
 
 # Download google cloud sdk install script
-curl https://sdk.cloud.google.com > install.sh
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
 bash ./aws/install -i $PATEFIANT_ROOT/opt -b $PATIEFIANT_ROOT/bin
