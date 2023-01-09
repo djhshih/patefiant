48f5705352dd75f4da170fc256043e5eba97ef8b
David J. H. Shih <djh.shih@gmail.com>
2023-01-09 23:40:04 +0800

    Update jdk.

--- pkg/jdk/install.sh
+++ pkg/jdk/install.sh
@@ -12,8 +12,8 @@ out_dir=$PATEFIANT_ROOT/opt
 
 tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
 
-# Download google cloud sdk install script
-wget -O ${name}.tar.gz --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/${name}_linux-x64_bin.tar.gz 
+#wget -O ${name}.tar.gz --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/${name}_linux-x64_bin.tar.gz 
+wget -O ${name}.tar.gz --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 mkdir -p $out_dir
 tar -xzvf $name.tar.gz -C $out_dir
 
