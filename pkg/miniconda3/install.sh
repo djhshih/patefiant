94a491a34e19d09c916594493dd5e9631fa1f9f7
David J. H. Shih <djh.shih@gmail.com>
2023-01-10 17:19:59 +0800

    Change miniconda3 to install bashrc.

--- pkg/miniconda3/install.sh
+++ pkg/miniconda3/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
+localdir=$(dirname "$(readlink -f "$0")")
+
 name=miniconda
 version=latest
 arch=$(uname -m)
@@ -21,6 +23,11 @@ tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
 
 curl -o installer.sh $url/Miniconda3-${version}-${platform}-${arch}.sh
 bash installer.sh
+
+# Install bashrc with expanded paths
+sed "s|\$PATEFIANT_ROOT|$PATEFIANT_ROOT|g" $localdir/bashrc |
+	sed "s|\$HOME|$HOME|g" \
+	> $HOME/.miniconda3.bashrc
 
 cd -
 
