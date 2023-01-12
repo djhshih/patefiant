89d009eeca447e6c05914837d5505c9c7d83ba91
David J. H. Shih <djh.shih@gmail.com>
2023-01-12 21:50:43 +0800

    Add frn.

---
+++ pkg/frn/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Script for installing R package installer rip
+
+name="frn"
+version="0.1"
+target_dir=$PATEFIANT_ROOT
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+# install rip
+curl -L https://github.com/djhshih/frn/releases/download/v${version}/frn-${version}.tar.bz2 |
+	tar --strip-components=1 -xj
+install $name $target_dir/bin
+
