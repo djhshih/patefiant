19216566acd9e6cba4b597ad5c101344261af3b9
David J. H. Shih <djh.shih@gmail.com>
2023-01-12 21:58:16 +0800

    Add igv.

---
+++ pkg/igv/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+localdir=$(dirname "$(readlink -f "$0")")
+
+name=igv
+version=2.15
+version_minor=4
+target_dir=$PATEFIANT_ROOT/opt/$name
+
+url=https://data.broadinstitute.org/igv/projects/downloads/${version}/IGV_${version}.${version_minor}.zip
+
+(
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+# Download and extract
+
+wget -O ${name}.zip $url
+unzip ${name}.zip && rm ${name}.zip
+
+mv IGV_${version}.${version_minor} $target_dir
+)
+
