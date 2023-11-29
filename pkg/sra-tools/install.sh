c1278e7af09a2b02e9a77fc1614823da8b61721f
David J. H. Shih <djh.shih@gmail.com>
2023-11-29 15:59:42 +0800

    Update sra-tools.

--- pkg/sra-tools/install.sh
+++ pkg/sra-tools/install.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-localdir=$(dirname "$(readlink -f "$0")")
-
-version=
+version=current-centos_linux64
 
 #tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
 target_dir=$PATEFIANT_ROOT/opt/sra-tools
@@ -12,6 +10,7 @@ target_dir=$PATEFIANT_ROOT/opt/sra-tools
 mkdir -p $target_dir && cd $target_dir
 
 # Download google cloud sdk install script
-curl -O https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz
+curl -O https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.$version.tar.gz
 tar -x --strip-components=1 -f sratoolkit.*.gz
+rm sratoolkit.*.gz
 
