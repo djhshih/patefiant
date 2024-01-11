83160c5b266e0d074d0e182a1a81c34fe3c7706a
David J. H. Shih <djh.shih@gmail.com>
2024-01-12 00:43:29 +0800

    Add dlazy.

---
+++ pkg/dlazy/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Script for installing R package installer rip
+
+name="rip"
+version="0.3"
+target_dir=$PATEFIANT_ROOT
+
+tmp_dir=$(mktemp -d) && cd $tmp_dir && echo $tmp_dir
+
+git clone --depth=1 https://github.com/djhshih/dlazy
+cd dlazy
+make install DESTDIR=$target_dir
+
